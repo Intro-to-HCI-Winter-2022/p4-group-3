19b7d3184891413c2fadae4ec191b3b006fa23a7
mackerson123 <maxwell.g.reiferson.22@dartmouth.edu>
2021-04-15 12:49:53 -0400

    netlify

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-console.log('starting up!');
+// const $ = require('jquery');
+import $ from 'jquery';
+
+import './style.scss';
+
+let num = 0;
+function secondCounter() {
+  $('#main').html('You&#39;ve been on this page for ' + num + ' seconds.');
+  num += 1;
+}
+
+setInterval(secondCounter, 1000);
