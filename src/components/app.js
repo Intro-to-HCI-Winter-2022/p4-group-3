96d943a4f0efdb10687a3de8bad244215b63c5dc
mackerson123 <maxwell.g.reiferson.22@dartmouth.edu>
2022-02-10 17:48:51 -0500

    removed unnecessary components from starterpack, general clean-up

--- src/components/app.js
+++ src/components/app.js
@@ -1,42 +1,10 @@
 import '../style.scss';
-
 import React from 'react';
 // import ReactDOM from 'react-dom';
 import {
-  BrowserRouter as Router, Route, NavLink, Switch,
+  BrowserRouter as Router, Route, Switch,
 } from 'react-router-dom';
-import Counter from './counter';
-import Controls from './controls';
-
-const About = (props) => {
-  return <div> All there is to know about me </div>;
-};
-const Welcome = (props) => {
-  return (
-    <div>
-      <div>Welcome</div>
-      <Counter />
-      <Controls />
-    </div>
-  );
-};
-
-const Nav = (props) => {
-  return (
-    <nav>
-      <ul>
-        <li><NavLink to="/" exact>Home</NavLink></li>
-        <li><NavLink to="/about">About</NavLink></li>
-        <li><NavLink to="/test/id1">test id1</NavLink></li>
-        <li><NavLink to="/test/id2">test id2</NavLink></li>
-      </ul>
-    </nav>
-  );
-};
-
-const Test = (props) => {
-  return <div> ID: {props.match.params.id} </div>;
-};
+import Home from '../pages/home';
 
 const FallBack = (props) => {
   return <div>URL Not Found</div>;
@@ -46,11 +14,8 @@ const App = (props) => {
   return (
     <Router>
       <div>
-        <Nav />
         <Switch>
-          <Route exact path="/" component={Welcome} />
-          <Route path="/about" component={About} />
-          <Route exact path="/test/:id" component={Test} />
+          <Route exact path="/" component={Home} />
           <Route component={FallBack} />
         </Switch>
       </div>
---
+++ src/pages/home.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+// eslint-disable-next-line react/prefer-stateless-function
+class Home extends Component {
+  // constructor(props) {
+  //     super(props);
+
+  //     this.state = {
+  //     }
+  // }
+
+  render() {
+    return (
+      <h1>Home</h1>
+    );
+  }
+}
+
+export default Home;
